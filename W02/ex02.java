9315288e7ac721bdbbf9ffc7fa41bf22b1817202
Egg94417 <dicky94417@gmail.com>
2026-03-05 11:12:14 +0800

    first push

---
+++ W02/ex02.java
@@ -0,0 +1,18 @@
+public class ex02 {
+    public static void main(String[] args){
+        // alt+shift+f
+        int grade = 80;
+        char type = 'm';
+        if(grade >= 60 && type == 'm') {
+            System.out.println("Cours:"+ type + "\n成績及格!"+grade);
+        } else {
+            System.out.println("課程不正確或成績不及格");
+
+        }
+    }
+    public static void main2(String[] args){
+        
+    }
+    }
+    
+
